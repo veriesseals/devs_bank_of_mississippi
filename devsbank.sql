22cc3557fa0786b9317c05ac442b44ddb91538f7
Veries Seals <veries@me.com>
2026-04-21 17:09:08 -0500

    Added  .gitignore and updated readme

--- devsbank.sql
+++
@@ -1,6 +0,0 @@
--- In MySQL, make a database (names can match your .env):
-
-CREATE DATABASE devsbank CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'root'@'%' IDENTIFIED BY 'Tyaanah201!';
-GRANT ALL PRIVILEGES ON devsbank.* TO 'root'@'%';
-FLUSH PRIVILEGES;
